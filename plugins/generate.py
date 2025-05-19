943ff05eab81909ad6427646a31a7dc5bb8c63ff
sundar998 <sundar19921@gmail.com>
2025-05-19 21:17:45 +0530

    Update generate.py

--- plugins/generate.py
+++ plugins/generate.py
@@ -22,10 +22,10 @@ mongo_client = MongoClient(DATABASE_URI)
 database = mongo_client.userdb.sessions
 
 strings = {
-    'need_login': "U have to /login before using then bot can download restricted content ❕",
-    'already_logged_in': "You are already logged in.\nIf you want to login again, /logout to proceed.",
+    'need_login': "You have to /login before using the bot to download restricted content ❕",
+    'already_logged_in': "You are already logged in.\nIf you want to login again, please use /logout to proceed.",
 }
-SESSION_STRING_SIZE = 351
+SESSION_STRING_SIZE = 351  # Minimum expected length for valid session strings
 
 def get(obj, key, default=None):
     try:
@@ -34,10 +34,10 @@ def get(obj, key, default=None):
         return default
 
 @Client.on_message(filters.private & filters.command(["logout"]) & filters.user(ADMIN))
-async def logout(_, msg):
+async def logout(_, msg: Message):
     user_data = database.find_one({"chat_id": msg.chat.id})
     if user_data is None or not user_data.get('session'):
-        return 
+        return await msg.reply("You are not logged in.")
     data = {
         'session': None,
         'logged_in': False
@@ -50,48 +50,90 @@ async def main(bot: Client, message: Message):
     user_data = database.find_one({"chat_id": message.from_user.id})
     if get(user_data, 'logged_in', False):
         await message.reply(strings['already_logged_in'])
-        return 
+        return
+
     user_id = int(message.from_user.id)
-    phone_number_msg = await bot.ask(chat_id=user_id, text="<b>Please send your phone number which includes country code</b>\n<b>Example:</b> <code>+13124562345, +9171828181889</code>")
-    if phone_number_msg.text=='/cancel':
-        return await phone_number_msg.reply('<b>process cancelled !</b>')
+
+    # Step 1: Ask for phone number
+    phone_number_msg = await bot.ask(
+        chat_id=user_id, 
+        text=(
+            "<b>Please send your phone number including country code</b>\n"
+            "<b>Example:</b> <code>+13124562345, +9171828181889</code>"
+        )
+    )
+    if phone_number_msg.text == '/cancel':
+        return await phone_number_msg.reply('<b>Process cancelled!</b>')
+
     phone_number = phone_number_msg.text
+
     client = Client(":memory:", API_ID, API_HASH)
     await client.connect()
+
     await phone_number_msg.reply("Sending OTP...")
+
     try:
         code = await client.send_code(phone_number)
-        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \n\nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.\n\n**Enter /cancel to cancel The Procces**", filters=filters.text, timeout=600)
     except PhoneNumberInvalid:
         await phone_number_msg.reply('`PHONE_NUMBER` **is invalid.**')
+        await client.disconnect()
         return
-    if phone_code_msg.text=='/cancel':
-        return await phone_code_msg.reply('<b>process cancelled !</b>')
+
+    # Step 2: Ask for OTP code
+    phone_code_msg = await bot.ask(
+        user_id, 
+        (
+            "Please check for an OTP in the official Telegram account. "
+            "If you got it, send OTP here after reading the format below.\n\n"
+            "If OTP is `12345`, **please send it as** `1 2 3 4 5`.\n\n"
+            "**Enter /cancel to cancel the process**"
+        ), 
+        filters=filters.text, 
+        timeout=600
+    )
+    if phone_code_msg.text == '/cancel':
+        await client.disconnect()
+        return await phone_code_msg.reply('<b>Process cancelled!</b>')
+
     try:
         phone_code = phone_code_msg.text.replace(" ", "")
         await client.sign_in(phone_number, code.phone_code_hash, phone_code)
     except PhoneCodeInvalid:
         await phone_code_msg.reply('**OTP is invalid.**')
+        await client.disconnect()
         return
     except PhoneCodeExpired:
         await phone_code_msg.reply('**OTP is expired.**')
+        await client.disconnect()
         return
     except SessionPasswordNeeded:
-        two_step_msg = await bot.ask(user_id, '**Your account has enabled two-step verification. Please provide the password.\n\nEnter /cancel to cancel The Procces**', filters=filters.text, timeout=300)
-        if two_step_msg.text=='/cancel':
-            return await two_step_msg.reply('<b>process cancelled !</b>')
+        # Two-step verification password required
+        two_step_msg = await bot.ask(
+            user_id,
+            '**Your account has two-step verification enabled. Please provide the password.\n\nEnter /cancel to cancel the process**',
+            filters=filters.text,
+            timeout=300
+        )
+        if two_step_msg.text == '/cancel':
+            await client.disconnect()
+            return await two_step_msg.reply('<b>Process cancelled!</b>')
+
         try:
             password = two_step_msg.text
             await client.check_password(password=password)
         except PasswordHashInvalid:
             await two_step_msg.reply('**Invalid Password Provided**')
+            await client.disconnect()
             return
+
+    # Export and save session string
     string_session = await client.export_session_string()
     await client.disconnect()
+
     if len(string_session) < SESSION_STRING_SIZE:
-        return await message.reply('<b>invalid session sring</b>')
+        return await message.reply('<b>Invalid session string</b>')
+
     try:
-        user_data = database.find_one({"chat_id": message.from_user.id})
         if user_data is not None:
             data = {
                 'session': string_session,
@@ -100,13 +142,19 @@ async def main(bot: Client, message: Message):
 
             uclient = Client(":memory:", session_string=data['session'], api_id=API_ID, api_hash=API_HASH)
             await uclient.connect()
+            # You might add test calls here if needed
+            await uclient.disconnect()
 
             database.update_one({'_id': user_data['_id']}, {'$set': data})
+        else:
+            # Insert new user if none found
+            database.insert_one({
+                'chat_id': message.from_user.id,
+                'session': string_session,
+                'logged_in': True
+            })
+
     except Exception as e:
         return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
-    await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout and /login again</b>")
 
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf you get any AUTH KEY errors, try /logout and then /login again</b>")
